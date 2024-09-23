f8d749fe0de1af68f3d73d39af2415f68952c06d
KAYKY-STORA-XOXOTA <143117283+KAYKY-STORA-XOXOTA@users.noreply.github.com>
2024-09-23 11:57:32 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Carros',
+    'O que é um motor V8?',
+    'Um motor V8 é um motor de combustão interna com oito cilindros dispostos em duas bancadas de quatro, formando um "V".'
+);
+
+criaCartao(
+    'Carros',
+    'Qual é a função de um turbo?',
+    'O turbo aumenta a potência do motor, comprimindo mais ar para dentro dos cilindros, permitindo uma queima mais eficiente de combustível.'
+);
+
+criaCartao(
+    'Carros',
+    'O que é um câmbio automático?',
+    'O câmbio automático é uma transmissão que muda as marchas automaticamente, sem a necessidade de intervenção do motorista.'
+);
+
+criaCartao(
+    'Carros',
+    'Qual a diferença entre tração dianteira e tração traseira?',
+    'Na tração dianteira, as rodas da frente movem o carro, enquanto na tração traseira, são as rodas traseiras que impulsionam o veículo.'
+);
